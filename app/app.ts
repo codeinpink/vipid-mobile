126054b11f8680762e1ec555c414cfaed38d6351
Shannon <shannon.babincsak@gmail.com>
2016-06-25 18:19:50 -0500

    Generate ContactList page

--- app/app.ts
+++ app/app.ts
@@ -2,13 +2,14 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
+import {ContactListPage} from './pages/contact-list/contact-list';
 
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage: any = ContactListPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
---
+++ app/pages/contact-list/contact-list.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ContactListPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/contact-list/contact-list.html',
+})
+export class ContactListPage {
+  constructor(private nav: NavController) {}
+}
