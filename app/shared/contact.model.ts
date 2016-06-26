bbe451a0fd6b6adad65ab6e3d7db05d1a826c537
Shannon <shannon.babincsak@gmail.com>
2016-06-26 15:39:35 -0500

    Add title field to Contact model and add title to contacts in ContactList

--- app/shared/contact.model.ts
+++ app/shared/contact.model.ts
@@ -1,6 +1,7 @@
 export class Contact {
     id: number;
     name: string;
+    title: string;
     company: string;
     location: string;
     picture: string;
--- app/shared/mock-contacts.ts
+++ app/shared/mock-contacts.ts
@@ -1,8 +1,8 @@
 import {Contact} from './contact.model';
 
 export var CONTACTS: Contact[] = [
-    {id: 1, name: 'John Doe', company: 'Paypal', location: 'Silicon Valley', picture: 'profile1.jpg'},
-    {id: 2, name: 'Francine Glass', company: 'Uber', location: 'San Francisco', picture: 'profile2.jpg'},
-    {id: 3, name: 'Matt Hooke', company: '', location: 'Chicago', picture: 'profile3.jpg'},
-    {id: 4, name: 'Samantha Jones', company: '', location: 'New York City', picture: 'profile4.jpg'},
+    {id: 1, name: 'John Doe', title: 'Director of Engineering', company: 'Paypal', location: 'Silicon Valley', picture: 'profile1.jpg'},
+    {id: 2, name: 'Francine Glass', title: 'Public Relations Expert', company: 'Uber', location: 'San Francisco', picture: 'profile2.jpg'},
+    {id: 3, name: 'Matt Hooke', title: 'CEO', company: 'Sunwave', location: 'Chicago', picture: 'profile3.jpg'},
+    {id: 4, name: 'Samantha Jones', title: 'Freelance Journalist', company: '', location: 'New York City', picture: 'profile4.jpg'},
 ];
