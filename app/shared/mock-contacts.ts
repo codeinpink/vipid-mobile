1bfc08b26389334683f2cf3e3c9027b162a464b7
Shannon <shannon.babincsak@gmail.com>
2016-06-26 15:31:20 -0500

    Add profile pictures to ContactList

--- app/shared/mock-contacts.ts
+++ app/shared/mock-contacts.ts
@@ -1,8 +1,8 @@
 import {Contact} from './contact.model';
 
 export var CONTACTS: Contact[] = [
-    {id: 1, name: 'John Doe', company: 'Paypal', location: 'Silicon Valley', picture: ''},
-    {id: 2, name: 'Francine Glass', company: 'Uber', location: 'San Francisco', picture: ''},
-    {id: 3, name: 'Matt Hooke', company: '', location: 'Chicago', picture: ''},
-    {id: 4, name: 'Samantha Jones', company: '', location: 'New York City', picture: ''},
+    {id: 1, name: 'John Doe', company: 'Paypal', location: 'Silicon Valley', picture: 'profile1.jpg'},
+    {id: 2, name: 'Francine Glass', company: 'Uber', location: 'San Francisco', picture: 'profile2.jpg'},
+    {id: 3, name: 'Matt Hooke', company: '', location: 'Chicago', picture: 'profile3.jpg'},
+    {id: 4, name: 'Samantha Jones', company: '', location: 'New York City', picture: 'profile4.jpg'},
 ];
