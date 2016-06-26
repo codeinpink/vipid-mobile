322bb1ce6ca05741d88d52818fe53feaa6393309
Shannon <shannon.babincsak@gmail.com>
2016-06-25 22:26:52 -0500

    Generate ContactDetail page

---
+++ app/pages/contact-detail/contact-detail.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ContactDetailPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/contact-detail/contact-detail.html',
+})
+export class ContactDetailPage {
+  constructor(private nav: NavController) {}
+}
