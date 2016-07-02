f2684217298c37528119101560ace06c0a983ea3
Shannon <shannon.babincsak@gmail.com>
2016-07-02 17:25:55 -0500

    Add addContact to ContactService
    This uses mockCreateContact for now.

--- app/shared/contact.service.ts
+++ app/shared/contact.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {CONTACTS} from './mock-contacts';
+import {Contact} from './contact.model';
 
 @Injectable()
 export class ContactService {
@@ -9,5 +10,20 @@ export class ContactService {
 
     getContact(id: number) {
         return Promise.resolve(this.getContacts().then(contacts => contacts.filter(contact => contact.id === id)[0]));
+    }
+
+    addContact(data) {
+        return Promise.resolve(this.mockCreateContact(data));
+    }
+
+    private mockCreateContact(data) {
+        let contact = new Contact();
+
+        contact.name = data.name;
+        contact.title = data.title;
+        contact.company = data.company;
+        contact.location = data.location;
+
+        CONTACTS.push(contact);
     }
 }
