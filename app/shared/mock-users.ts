8bd14716b43edc8393497d4acce7b9df7faa8119
Shannon <shannon.babincsak@gmail.com>
2016-07-04 16:33:53 -0500

    Add picture field to User model

--- app/shared/mock-users.ts
+++ app/shared/mock-users.ts
@@ -1,8 +1,8 @@
 import { User } from './user.model';
 
 export var USERS: User[] = [
-    {id: 1, name: 'John Doe', email: 'john@doe.com', phone: 1111111111},
-    {id: 2, name: 'Francine Glass', email: 'francine@glass.com', phone: 2222222222},
-    {id: 3, name: 'Matt Hooke', email: 'matt@hooke.com', phone: 3333333333},
-    {id: 4, name: 'Samantha Jones', email: 'samantha@jones.com', phone: 4444444444}
+    {id: 1, name: 'John Doe', email: 'john@doe.com', phone: 1111111111, picture: 'profile1.jpg'},
+    {id: 2, name: 'Francine Glass', email: 'francine@glass.com', phone: 2222222222, picture: 'profile2.jpg'},
+    {id: 3, name: 'Matt Hooke', email: 'matt@hooke.com', phone: 3333333333, picture: 'profile3.jpg'},
+    {id: 4, name: 'Samantha Jones', email: 'samantha@jones.com', phone: 4444444444, picture: 'profile4.jpg'}
 ];
