de0b7d7a543da40acbf797e9ee5ac8dec50272a1
Shannon <shannon.babincsak@gmail.com>
2016-07-04 08:55:55 -0500

    Generate ContactAddMenu page

---
+++ app/pages/contact-add-menu/contact-add-menu.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ContactAddMenuPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/contact-add-menu/contact-add-menu.html',
+})
+export class ContactAddMenuPage {
+  constructor(private nav: NavController) {}
+}
