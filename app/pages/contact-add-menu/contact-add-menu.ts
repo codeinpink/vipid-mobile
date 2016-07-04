4f00d96114b414846bf97f97c5c9e90f1a298fa3
Shannon <shannon.babincsak@gmail.com>
2016-07-04 09:25:07 -0500

    Add buttons to ContactAddMenu w/ nav to ContactCreate

--- app/pages/contact-add-menu/contact-add-menu.ts
+++ app/pages/contact-add-menu/contact-add-menu.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ContactCreatePage } from '../contact-create/contact-create';
 
 /*
   Generated class for the ContactAddMenuPage page.
@@ -11,5 +12,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/contact-add-menu/contact-add-menu.html',
 })
 export class ContactAddMenuPage {
-  constructor(private nav: NavController) {}
+    constructor(private nav: NavController) {}
+
+    onFindContactClick() {
+        console.log('onFindContactClick');
+    }
+
+    onCreateContactClick() {
+        this.nav.push(ContactCreatePage);
+    }
 }
