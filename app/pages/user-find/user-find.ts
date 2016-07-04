b682cfb6c6a7e5487b6583c7c596eb06b75e2390
Shannon <shannon.babincsak@gmail.com>
2016-07-04 12:57:11 -0500

    Add form to UserFind

--- app/pages/user-find/user-find.ts
+++ app/pages/user-find/user-find.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, ControlGroup, Validators } from '@angular/common';
 
 /*
   Generated class for the UserFindPage page.
@@ -10,6 +11,19 @@ import { NavController } from 'ionic-angular';
 @Component({
   templateUrl: 'build/pages/user-find/user-find.html',
 })
-export class UserFindPage {
-  constructor(private nav: NavController) {}
+export class UserFindPage implements OnInit {
+    searchForm: ControlGroup;
+
+    constructor(private nav: NavController, private formBuilder: FormBuilder) {}
+
+    onSubmit(data) {
+        console.log(data);
+    }
+
+    ngOnInit() {
+        this.searchForm = this.formBuilder.group({
+            email: [],
+            phone: []
+        });
+    }
 }
