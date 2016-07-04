2826c6342c0d8c5cf4709afe4af9b5e35199569b
Shannon <shannon.babincsak@gmail.com>
2016-07-04 12:40:15 -0500

    Add UserService with mock users

---
+++ app/shared/mock-users.ts
@@ -0,0 +1,8 @@
+import { User } from './user.model';
+
+export var USERS: User[] = [
+    {id: 1, name: 'John Doe', email: 'john@doe.com', phone: 1111111111},
+    {id: 2, name: 'Francine Glass', email: 'francine@glass.com', phone: 2222222222},
+    {id: 3, name: 'Matt Hooke', email: 'matt@hooke.com', phone: 3333333333},
+    {id: 4, name: 'Samantha Jones', email: 'samantha@jones.com', phone: 4444444444}
+];
