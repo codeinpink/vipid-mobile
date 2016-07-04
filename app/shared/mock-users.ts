6d4f436a73c32a62fdaeaaa72c88db1ce75513fc
Shannon <shannon.babincsak@gmail.com>
2016-07-04 16:37:55 -0500

    Add the rest of the contact fields to the User model

--- app/shared/mock-users.ts
+++ app/shared/mock-users.ts
@@ -1,8 +1,8 @@
 import { User } from './user.model';
 
 export var USERS: User[] = [
-    {id: 1, name: 'John Doe', email: 'john@doe.com', phone: 1111111111, picture: 'profile1.jpg'},
-    {id: 2, name: 'Francine Glass', email: 'francine@glass.com', phone: 2222222222, picture: 'profile2.jpg'},
-    {id: 3, name: 'Matt Hooke', email: 'matt@hooke.com', phone: 3333333333, picture: 'profile3.jpg'},
-    {id: 4, name: 'Samantha Jones', email: 'samantha@jones.com', phone: 4444444444, picture: 'profile4.jpg'}
+    {id: 1, name: 'John Doe', title: 'Director of Engineering', company: 'Paypal', location: 'Silicon Valley', email: 'john@doe.com', phone: 1111111111, picture: 'profile1.jpg'},
+    {id: 2, name: 'Francine Glass', title: 'Public Relations Expert', company: 'Uber', location: 'San Francisco', email: 'francine@glass.com', phone: 2222222222, picture: 'profile2.jpg'},
+    {id: 3, name: 'Matt Hooke', title: 'CEO', company: 'Sunwave', location: 'Chicago', email: 'matt@hooke.com', phone: 3333333333, picture: 'profile3.jpg'},
+    {id: 4, name: 'Samantha Jones', title: 'Freelance Journalist', company: '', location: 'New York City', email: 'samantha@jones.com', phone: 4444444444, picture: 'profile4.jpg'}
 ];
--- app/shared/user.model.ts
+++ app/shared/user.model.ts
@@ -1,6 +1,9 @@
 export class User {
     id: number;
     name: string;
+    title: string;
+    company: string;
+    location: string;
     email: string;
     phone: number;
     picture: string;
