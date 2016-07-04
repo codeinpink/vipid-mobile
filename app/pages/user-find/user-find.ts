1a89ce71c163d230c841c5b8fe32d98a26a2bf4c
Shannon <shannon.babincsak@gmail.com>
2016-07-04 12:42:38 -0500

    Generate UserFind page

---
+++ app/pages/user-find/user-find.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the UserFindPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/user-find/user-find.html',
+})
+export class UserFindPage {
+  constructor(private nav: NavController) {}
+}
