4f811e5ce79374e7138cd32071c9a29200daf179
Shannon <shannon.babincsak@gmail.com>
2016-07-04 14:06:37 -0500

    Add getUserByEmailOrPhone to UserService

--- app/shared/user.service.ts
+++ app/shared/user.service.ts
@@ -6,4 +6,19 @@ export class UserService {
     getUsers() {
         return Promise.resolve(USERS);
     }
+
+    getUserByEmailOrPhone(email: string, phone: number) {
+        return Promise.resolve(this.getUsers().then(users => {
+            let user = null;
+
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].email === email || users[i].phone === phone) {
+                    user = users[i];
+                    break;
+                }
+            }
+
+            return user;
+        }));
+    }
 }
