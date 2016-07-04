ea3508c5db8fe1cdbdce7639700adbfed2034536
Shannon <shannon.babincsak@gmail.com>
2016-07-04 12:44:38 -0500

    Add navigation from ContactAddMenu to UserFindPage

--- app/pages/contact-add-menu/contact-add-menu.ts
+++ app/pages/contact-add-menu/contact-add-menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ContactCreatePage } from '../contact-create/contact-create';
+import { UserFindPage } from '../user-find/user-find';
 
 /*
   Generated class for the ContactAddMenuPage page.
@@ -15,7 +16,7 @@ export class ContactAddMenuPage {
     constructor(private nav: NavController) {}
 
     onFindContactClick() {
-        console.log('onFindContactClick');
+        this.nav.push(UserFindPage);
     }
 
     onCreateContactClick() {
