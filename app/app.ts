ebb15bd3658080ae8101b3461da12502a16a770c
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-08-18 23:52:19 -0500

    Add phonegap-nfc
    Who knows if it works, though...

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,14 @@
+//<reference path="../../typings/cordova/cordova.d.ts" />
+///<reference path="../typings/globals/phonegap-nfc/index.d.ts" />
+
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {ContactListPage} from './pages/contact-list/contact-list';
 
+
+declare var nfc: any;
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -16,6 +21,8 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+      //nfc.addMimeTypeListener("text/json", {}, {}, {});
     });
   }
 }
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="globals/phonegap-nfc/index.d.ts" />
