91de7020d88081c62b589d584f05ace065fd3342
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-08-19 08:26:41 -0500

    Add Group model and service

---
+++ app/shared/group.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {Group} from './group.model';
+import {GROUPS} from './mock-groups';
+
+@Injectable()
+export class GroupService {
+    getGroups() {
+        return Promise.resolve(GROUPS);
+    }
+
+    getContact(id: number) {
+        return Promise.resolve(this.getGroups().then(groups => groups.filter(group => group.id === id)[0]));
+    }
+}
---
+++ app/shared/mock-groups.ts
@@ -0,0 +1,6 @@
+import {Group} from './group.model';
+
+export var GROUPS: Group[] = [
+    {id: 1, name: 'Co-workers', owner: 1, contacts: []},
+    {id: 2, name: 'DjangoCon', owner: 1, contacts: []},
+];
