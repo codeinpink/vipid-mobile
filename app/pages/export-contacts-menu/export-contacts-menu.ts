6bf0cabce862b372dec50ade955c62f861b38aa3
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-09-24 11:59:03 -0500

    Add ExportContactsMenu page

---
+++ app/pages/export-contacts-menu/export-contacts-menu.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+    templateUrl: 'build/pages/export-contacts-menu/export-contacts-menu.html',
+})
+export class ExportContactsMenuPage {
+    constructor(private nav: NavController) {}
+
+    onPhoneContactsClick() {
+        
+    }
+}
--- app/pages/import-contacts-menu/import-contacts-menu.ts
+++ app/pages/import-contacts-menu/import-contacts-menu.ts
@@ -6,16 +6,8 @@ import {ImportPhoneContactsPage} from '../import-phone-contacts/import-phone-con
     templateUrl: 'build/pages/import-contacts-menu/import-contacts-menu.html',
 })
 export class ImportContactsMenuPage {
-    pages: any;
-
     constructor(private nav: NavController) {
 
-    }
-
-    openPage(page) {
-        // Reset the content nav to have just this page
-        // we wouldn't want the back button to show in this scenario
-        this.nav.setRoot(page.component);
     }
 
     onPhoneContactsClick() {
