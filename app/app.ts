85b253f7f18d22c7d79c8bc5160cc66c8f64d28e
Shannon Babincsak <shannon.babincsak@gmail.com>
2016-09-23 21:40:38 -0500

    Add side menu

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
 //<reference path="../../typings/cordova/cordova.d.ts" />
 ///<reference path="../typings/globals/phonegap-nfc/index.d.ts" />
 
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, ionicBootstrap, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {ContactListPage} from './pages/contact-list/contact-list';
@@ -11,20 +11,31 @@ import {ContactListPage} from './pages/contact-list/contact-list';
 declare var nfc: any;
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
-  rootPage: any = ContactListPage;
+    @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+    rootPage: any = ContactListPage;
+    pages: any;
 
-      //nfc.addMimeTypeListener("text/json", {}, {}, {});
-    });
-  }
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+
+            //nfc.addMimeTypeListener("text/json", {}, {}, {});
+        });
+
+
+        this.pages = [
+            {title: 'Contacts', component: ContactListPage },
+            {title: 'Import', component: {}},
+            {title: 'Export', component: {}},
+            {title: 'Settings', component: {}}
+        ];
+    }
 }
 
 ionicBootstrap(MyApp);
