2e6ecebcf4e8725ea106039353fb0021b33c0308
Shannon Babincsak <shannon.babincsak@gmail.com>
2017-02-08 10:28:25 -0600

    Fix group service URL

--- app/shared/group.service.ts
+++ app/shared/group.service.ts
@@ -6,7 +6,7 @@ import {GROUPS} from './mock-groups';
 
 @Injectable()
 export class GroupService {
-    private groupsUrl = 'http://localhost:8000/api/contacts/';
+    private groupsUrl = 'http://localhost:8000/api/groups/';
     private grouptDetailUrl = this.groupsUrl + '{ID}' + '/';
 
     constructor(private http: Http) {}
