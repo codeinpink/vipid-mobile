5e698103e3c650127fee932ccaf2c3106e648fb1
Shannon Babincsak <shannon.babincsak@gmail.com>
2017-02-25 12:16:58 -0600

    Add QR Scan option for adding contacts

--- app/pages/contact-add-menu/contact-add-menu.ts
+++ app/pages/contact-add-menu/contact-add-menu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { ContactCreatePage } from '../contact-create/contact-create';
 import { UserFindPage } from '../user-find/user-find';
 import { NfcAddPage } from '../nfc-add/nfc-add';
+import { BarcodeScanner } from 'ionic-native';
 
 /*
   Generated class for the ContactAddMenuPage page.
@@ -14,7 +15,7 @@ import { NfcAddPage } from '../nfc-add/nfc-add';
   templateUrl: 'build/pages/contact-add-menu/contact-add-menu.html',
 })
 export class ContactAddMenuPage {
-    constructor(private nav: NavController) {}
+    constructor(private nav: NavController, private alertCtrl: AlertController) {}
 
     onFindContactClick() {
         this.nav.push(UserFindPage);
@@ -26,5 +27,24 @@ export class ContactAddMenuPage {
 
     onNFCAddClick() {
         this.nav.push(NfcAddPage);
+    }
+
+    onQRScanClick() {
+        BarcodeScanner.scan({
+            formats: "QR_CODE",
+            prompt: "Place a QR code inside the scan area"
+        }).then((barcodeData) => {
+            if (barcodeData.cancelled === false) {
+                console.log(barcodeData.text);
+            }
+        }, (error) => {
+            let alert = this.alertCtrl.create({
+                title: 'Error',
+                subTitle: 'The QR Scanner could not be opened.',
+                buttons: ['OK']
+            });
+
+            alert.present();
+        });
     }
 }
