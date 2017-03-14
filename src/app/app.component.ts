0fec0f44622d67dbb70834eb58e384291c5a4f6c
Shannon Babincsak <shannon.babincsak@gmail.com>
2017-03-14 11:38:31 -0500

    Check for unauth responses and redirect to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import  {MyProfileEditPage } from '../pages/my-profile-edit/my-profile-edit';
+import { MyProfileEditPage } from '../pages/my-profile-edit/my-profile-edit';
 import { ContactListPage } from '../pages/contact-list/contact-list';
 import { GroupListPage } from '../pages/group-list/group-list';
 import { ImportContactsMenuPage } from '../pages/import-contacts-menu/import-contacts-menu';
 import { ExportContactsPage } from '../pages/export-contacts/export-contacts';
 import { UserSettingsPage } from '../pages/user-settings/user-settings';
 import { LoginPage } from '../pages/login/login';
+import { HttpService } from '../shared/http.service';
 
 
 @Component({
@@ -20,12 +21,18 @@ export class MyApp {
   pages: any;
   profile: any;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, http: HttpService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+    });
+
+    http.isUnauthenticated.subscribe(_ => {
+        if (!this.nav.isTransitioning()) {
+            this.nav.setRoot(LoginPage);
+        }
     });
 
     this.profile = {component: MyProfileEditPage};
