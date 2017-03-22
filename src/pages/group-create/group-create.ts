512f0ffcabf2c6add29d3a3b3c9de9d9a5becd2a
Shannon Babincsak <shannon.babincsak@gmail.com>
2017-03-22 11:31:10 -0500

    Add group edit (using create page) and delete

--- src/pages/group-create/group-create.ts
+++ src/pages/group-create/group-create.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Group } from '../../shared/group.model';
 import { GroupService } from '../../shared/group.service';
+import { NotificationManager } from '../../providers/notification-manager/notification-manager';
 
 
 @Component({
@@ -9,15 +11,46 @@ import { GroupService } from '../../shared/group.service';
     providers: [FormBuilder, GroupService]
 })
 export class GroupCreatePage {
+    mode: string = 'CREATE';
+    title: string = 'Create Group';
+    submitText: string = 'Create Group';
+    group: Group;
     groupForm: FormGroup;
 
-    constructor(private nav: NavController, private formBuilder: FormBuilder, private groupService: GroupService) {
-        this.groupForm = this.formBuilder.group({
-            name: ['', Validators.required]
-        });
+    constructor(private nav: NavController, private navParams: NavParams, private fb: FormBuilder, private groupService: GroupService,
+    private nm: NotificationManager) {
+
     }
 
     onSubmit(group) {
-        this.groupService.createGroup(group).subscribe(_ => this.nav.pop());
+        if (this.groupForm.valid && this.groupForm.dirty) {
+            if (this.mode === 'CREATE') {
+                this.groupService.createGroup(group).subscribe(_ => {
+                    this.nav.pop().then(_ => this.nm.showSuccessMessage('Group created'));
+                });
+            } else {
+                this.group.name = group.name;
+                this.groupService.updateGroup(this.group).subscribe(_ => {
+                    this.nav.pop().then(_ => this.nm.showSuccessMessage('Group updated'));
+                });
+            }
+        }
+    }
+
+    ngOnInit() {
+        let group = this.navParams.get('group');
+
+        if (group) {
+            this.mode = 'UPDATE';
+            this.title = 'Update Group';
+            this.submitText = 'Update Group';
+            this.group = group;
+        } else {
+            this.group = new Group();
+        }
+
+        this.groupForm = this.fb.group({
+            name: [this.group.name, Validators.compose([Validators.required, Validators.maxLength(50)])]
+        });
     }
 }
--- src/pages/group-detail/group-detail.ts
+++ src/pages/group-detail/group-detail.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { Group } from '../../shared/group.model';
 import { Contact } from '../../shared/contact.model';
 import { ContactDetailPage } from '../contact-detail/contact-detail';
 import { GroupContactSelectPage } from '../group-contact-select/group-contact-select';
+import { GroupService } from '../../shared/group.service';
+import { NotificationManager } from '../../providers/notification-manager/notification-manager';
+import { GroupCreatePage } from '../group-create/group-create';
 
 
 @Component({
@@ -12,8 +15,38 @@ import { GroupContactSelectPage } from '../group-contact-select/group-contact-se
 export class GroupDetailPage {
     group: Group;
 
-    constructor(private navCtrl: NavController, private navParams: NavParams) {
+    constructor(private navCtrl: NavController, private navParams: NavParams, private groupService: GroupService,
+    private alertCtrl: AlertController, private nm: NotificationManager) {
 
+    }
+
+    onEditClick() {
+        this.navCtrl.push(GroupCreatePage, {
+            group: this.group
+        });
+    }
+
+    onDeleteClick() {
+        let alert = this.alertCtrl.create({
+            title: 'Delete Contact',
+            message: 'Are you sure you want to delete this group?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: () => {}
+                },
+                {
+                    text: 'Delete',
+                    handler: () => {
+                        this.groupService.deleteGroup(this.group).subscribe(_ => {
+                            this.navCtrl.pop().then(_ => this.nm.showSuccessMessage('Group deleted'));
+                        });
+                    }
+                }
+            ]
+        });
+
+        alert.present();
     }
 
     onAddClick() {
